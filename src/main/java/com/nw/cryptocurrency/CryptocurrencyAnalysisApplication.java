8b1a81052d517d333d71e1476a49f68f541211de
Nicholas <nikolaj.belyakov.84@mail.ru>
2022-09-23 19:22:55 +0300

    initial commit

---
+++ src/main/java/com/nw/cryptocurrency/CryptocurrencyAnalysisApplication.java
@@ -0,0 +1,13 @@
+package com.nw.cryptocurrency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptocurrencyAnalysisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptocurrencyAnalysisApplication.class, args);
+	}
+
+}
